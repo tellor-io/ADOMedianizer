645c2158767a3f6b6487ca88a453cf334330a7de
Brenda <l.loya.b@gmail.com>
2020-04-10 13:11:55 -0400

    comments

--- contracts/ADOMedianizer.sol
+++ contracts/ADOMedianizer.sol
@@ -3,58 +3,83 @@ import "./EIP2362Interface.sol";
 import "./SafeMath.sol";
 
 contract ADOMedianizer is EIP2362Interface{
+    /*Variables*/ 
+    address public owner;
+    address[] public oracles;
+    mapping(address => uint) index;
 
-  address public owner;
-  address[] public oracles;
-  mapping(address => uint) index;
-
-
-  constructor() public {
-    owner = msg.sender;
-    oracles.push(address(0));
-  }
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function changeOwner(address _newOwner) restricted() public{
-    owner = _newOwner;
-  }
-
-  //This is the exposed EIP function that loops through the oracle addresses and medianizes the values
-  function valueFor(bytes32 _id) external view returns(int,uint,uint){
-    int[] memory values;
-    int _val;
-    uint _time;
-    uint _status;
-    uint len = 0;
-    for(uint i = 0; i< oracles.length;i++){
-      (_val,_time,_status) = EIP2362Interface(oracles[i]).valueFor(_id);
-      if(_status == 200){
-        values[len] = _val;
-        len++;
-      }
+  
+    constructor() public {
+        owner = msg.sender;
+        oracles.push(address(0));
     }
-    return (median(values),now,200);
+
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    /*Functions*/
+    /**
+    * @dev allows owner to transfer ownership
+    * @param _newOwner is the address ownership is being transferred to
+    */
+    function changeOwner(address _newOwner) restricted() public{
+        owner = _newOwner;
+    }
+
+    /**
+    * @dev This function loops through the oracle addresses and medianizes the values
+    * @param _id the standarized ADO data type/value pair id
+    * @return median, timestamp and status
+    */
+    function valueFor(bytes32 _id) external view returns(int,uint,uint){
+        int[] memory values;
+        int _val;
+        uint _time;
+        uint _status;
+        uint len = 0;
+        for(uint i = 0; i< oracles.length;i++){
+           (_val,_time,_status) = EIP2362Interface(oracles[i]).valueFor(_id);
+              if(_status == 200){
+                  values[len] = _val;
+                  len++;
+              }
+        }
+        return (median(values),now,200);
   } 
 
-  function addOracle(address _oracle) restricted() external{
-    index[_oracle] = oracles.length;
-    oracles.push(_oracle);
-  }
-
-  function removeOracle(address _oracle) restricted() external{
-    uint _index = index[_oracle];
-    if(_index != oracles.length){
-      address last = oracles[oracles.length - 1];
-      oracles[_index] = last;
+    /**
+    * @dev Allows owner to add an oracle provider
+    * @param _oracle is the oracle address for the oracle being added
+    */
+    function addOracle(address _oracle) restricted() external{
+        index[_oracle] = oracles.length;
+        oracles.push(_oracle);
     }
-    oracles.length--;
-    index[_oracle] = 0;
-  }
 
-  function median(int256[] memory a) internal view returns(int){
+
+    /**
+    * @dev Allows the owner to remove an oracle provider
+    * @param _oracle is the oracle address for the oracle being excluded
+    */
+    function removeOracle(address _oracle) restricted() external{
+        uint _index = index[_oracle];
+        if(_index != oracles.length){
+            address last = oracles[oracles.length - 1];
+            oracles[_index] = last;
+        }
+        oracles.length--;
+        index[_oracle] = 0;
+    }
+
+
+    /**
+    * @dev Internal function that sorts values submitted by oracles and returns the median
+    * @param a is the array containing the values submitted for the Id by all the oracles
+    * @return median value
+    */
+    function median(int256[] memory a) internal view returns(int){
         uint256 i;
         for (i = 1; i < a.length; i++) {
             int256 temp = a[i];
@@ -67,6 +92,8 @@ contract ADOMedianizer is EIP2362Interface{
                 a[j]= temp;
             }
         }
-  }
+        uint256 m = a.length/2;
+        returns(a[m]);
+    }
 
 }
--- contracts/testContracts/sampleOracle.sol
+++ contracts/testContracts/sampleOracle.sol
@@ -1,24 +1,38 @@
 pragma solidity >=0.5.0 <0.7.0;
 import "../EIP2362Interface.sol";
 
+/**
+* @title sampleOracle
+* 
+*/
+
 contract sampleOracle is EIP2362Interface{
-  mapping(bytes32=>mapping(uint=>int256)) valuesByID;
-  mapping (bytes32 => uint[]) timestampsByID;
+    /*Variables*/
+    mapping(bytes32=>mapping(uint=>int256)) valuesByID;
+    mapping (bytes32 => uint[]) timestampsByID;
   
+    /**
+    * @dev Allows oracle provider to push the vlue for the corresponding _id
+    * @param _id is the standarized ADO data type/value pair Id
+    * @param _value is the value 
+    */
+    function setValue(bytes32 _id, int256 _value) external{
+      	valuesByID[_id][now] = _value;
+      	timestampsByID[_id].push(now);
+    }
 
-  function setValue(bytes32 _id, int256 _value) external{
-  	valuesByID[_id][now] = _value;
-  	timestampsByID[_id].push(now);
-  }
-
-
-
-  //This is the exposed EIP function that loops through the oracle addresses and medianizes the values
-  function valueFor(bytes32 _id) external view returns(int _value,uint _timestamp,uint _status){
-    _timestamp = timestampsByID[_id][timestampsByID[_id].length - 1];
-    if(_timestamp > 0){
-    	return(valuesByID[_id][_timestamp],_timestamp,200);
-	}
-	else return(0,0,404);
-  } 
+    /**
+    * @dev Allows the ADOMedianizer to retreive the value, timestamp, and status for the specifed Id
+    * @param _id is the standarized ADO data type/value pair id
+    * @return _value the value corresponding to the id
+    * @return _timestamp is the unix timestamp
+    * @return _status is the standarized data status
+    */
+    function valueFor(bytes32 _id) external view returns(int _value,uint _timestamp,uint _status){
+        _timestamp = timestampsByID[_id][timestampsByID[_id].length - 1];
+        if(_timestamp > 0){
+           	return(valuesByID[_id][_timestamp],_timestamp,200);
+	      }
+	      else return(0,0,404);
+    } 
 }
